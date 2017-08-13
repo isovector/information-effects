e4ad8573b037937eacfaf26434b58872d17c9de5
Sandy Maguire <sandy@sandymaguire.me>
2017-08-12 21:12:20 -0600

    let's use our own types

--- src/Main.hs
+++ src/Main.hs
@@ -3,161 +3,162 @@
 {-# LANGUAGE PatternSynonyms      #-}
 {-# LANGUAGE RebindableSyntax     #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module Main where
 
-import Prelude (IO, putStrLn, Either (..), (.), Num (..), ($), error, show, Int, fst, snd)
-import qualified Prelude as P
-import qualified Prelude as Dbg (String, (++))
 import qualified Debug.Trace as Dbg
+import           Prelude (IO, putStrLn, (.), Num (..), ($), error, show, Int, fst, snd)
+import qualified Prelude as Dbg (String, (++))
+import qualified Prelude as P
 
-type Bool = Either () ()
+data U = U
 
-pattern True :: Bool
-pattern True = Left ()
+type Bool = U + U
 
-pattern False :: Bool
-pattern False = Right ()
+infix 3 <=>
 
-data Iso a b = Iso
+data a <=> b
+  = Iso
   { to   :: a -> b
   , from :: b -> a
   }
 
+data a * b = Pair a b
+data a + b
+  = InL a
+  | InR b
+
+type Iso    = (<=>)
 type Iso' a = Iso a a
 
-(>>) :: Iso a b -> Iso b c -> Iso a c
-(>>) = trans
+return :: a <=> a
+return = id
 
-return = error "not applicable here, sucka"
-
-(|>) :: Iso a b -> Iso b c -> Iso a c
-(|>) = trans
-
-swapProd :: Iso (a, b) (b, a)
-swapProd = Iso swap' swap'
+swapProd :: a * b <=> b * a
+swapProd = Iso swap swap
   where
-    swap' (a, b) = (b, a)
+    swap (Pair a b) = (Pair b a)
 
-swapCoprod :: Iso (Either a b) (Either b a)
-swapCoprod = Iso swap' swap'
+swapCoprod :: a + b <=> b + a
+swapCoprod = Iso swap swap
   where
-    swap' (Left a)  = Right a
-    swap' (Right b) = Left b
+    swap (InL a) = InR a
+    swap (InR b) = InL b
 
-assocCoprod :: Iso (Either a (Either b c)) (Either (Either a b) c)
+assocCoprod :: (a + (b + c)) <=> ((a + b) + c)
 assocCoprod = Iso assocl assocr
   where
-    assocl (Left a)          = Left (Left a)
-    assocl (Right (Left b))  = Left (Right b)
-    assocl (Right (Right c)) = (Right c)
+    assocl (InL a)       = InL (InL a)
+    assocl (InR (InL b)) = InL (InR b)
+    assocl (InR (InR c)) = InR c
 
-    assocr (Left (Left a))  = (Left a)
-    assocr (Left (Right b)) = (Right (Left b))
-    assocr (Right c)        = (Right (Right c))
+    assocr (InL (InL a)) = InL a
+    assocr (InL (InR b)) = InR (InL b)
+    assocr (InR c)       = InR (InR c)
 
-assocProd :: Iso (a, (b, c)) ((a, b), c)
+assocProd :: (a * (b * c)) <=> ((a * b) * c)
 assocProd = Iso assocl assocr
   where
-    assocl (a, (b, c)) = ((a, b), c)
-    assocr ((a, b), c) = (a, (b, c))
+    assocl (Pair a (Pair b c)) = (Pair (Pair a b) c)
+    assocr (Pair (Pair a b) c) = (Pair a (Pair b c))
 
-unit :: Iso ((), a) a
+unit :: (U * a) <=> a
 unit = Iso unite uniti
   where
-    unite ((), a) = a
-    uniti a = ((), a)
+    unite (Pair U a) = a
+    uniti a = (Pair U a)
 
-distrib :: Iso (Either a b, c) (Either (a, c) (b, c))
+distrib :: (a + b * c) <=> (a * c) + (b * c)
 distrib = Iso distrib factor
   where
-    distrib (Left a, c) = Left (a, c)
-    distrib (Right b, c) = Right (b, c)
+    distrib (Pair (InL a) c) = InL (Pair a c)
+    distrib (Pair (InR b) c) = InR (Pair b c)
 
-    factor (Left (a, c))  = (Left a, c)
-    factor (Right (b, c)) = (Right b, c)
+    factor (InL (Pair a c)) = (Pair (InL a) c)
+    factor (InR (Pair b c)) = (Pair (InR b) c)
 
 sym :: Iso a b -> Iso b a
 sym iso = Iso (from iso) (to iso)
 
-trans :: Iso a b -> Iso b c -> Iso a c
-trans ab bc = Iso (to bc . to ab) (from ab . from bc)
+(>>) :: Iso a b -> Iso b c -> Iso a c
+(>>) ab bc = Iso (to bc . to ab) (from ab . from bc)
 
 id :: Iso' a
 id = Iso (\a -> a) (\a -> a)
 
-parProd :: Iso a b -> Iso c d -> Iso (a, c) (b, d)
-parProd ab cd = Iso (\(a, c) -> (to ab a, to cd c))
-                    (\(b, d) -> (from ab b, from cd d))
+parProd :: Iso a b -> Iso c d -> Iso (a * c) (b * d)
+parProd ab cd = Iso (\(Pair a c) -> (Pair (to ab a) (to cd c)))
+                    (\(Pair b d) -> (Pair (from ab b) (from cd d)))
 
-parCoprod :: Iso a b -> Iso c d -> Iso (Either a c) (Either b d)
+parCoprod :: Iso a b -> Iso c d -> Iso (a + c) (b + d)
 parCoprod ab cd = Iso to' from'
   where
-    to' (Left a)    = Left (to ab a)
-    to' (Right c)   = Right (to cd c)
-    from' (Left b)  = Left (from ab b)
-    from' (Right d) = Right (from cd d)
+    to' (InL a)   = InL (to ab a)
+    to' (InR c)   = InR (to cd c)
+    from' (InL b) = InL (from ab b)
+    from' (InR d) = InR (from cd d)
 
-newtype Fix f = Fix { unfold' :: f (Fix f) }
+newtype Fix f = Fix { unFix :: f (Fix f) }
 
-fold :: Iso (f (Fix f)) (Fix f)
-fold = Iso Fix unfold'
+fold :: (f (Fix f)) <=> (Fix f)
+fold = Iso Fix unFix
 
-type Nat = Fix (Either ())
+type Nat = Fix ((+) U)
 
 instance P.Show Nat where
   show f = show $ nat2Int f
     where
-      nat2Int (Fix (Left ())) = 0
-      nat2Int (Fix (Right n)) = 1 + nat2Int n
+      nat2Int (Fix (InL U)) = 0
+      nat2Int (Fix (InR n)) = 1 + nat2Int n
 
 
-trace :: forall a b c. Iso (Either a b) (Either a c) -> Iso b c
-trace comb = Iso (\b -> loopfwd (to comb (Right b)))
-                 (\c -> loopbwd (from comb (Right c)))
+trace :: forall a b c. Iso (a + b) (a + c) -> Iso b c
+trace comb = Iso (\b -> loopfwd (to comb (InR b)))
+                 (\c -> loopbwd (from comb (InR c)))
   where
-    loopfwd :: Either a c -> c
-    loopfwd (Left a) = loopfwd (to comb (Left a))
-    loopfwd (Right c) = c
+    loopfwd :: a + c -> c
+    loopfwd (InL a) = loopfwd (to comb (InL a))
+    loopfwd (InR c) = c
 
-    loopbwd :: Either a b -> b
-    loopbwd (Left a) = loopbwd (from comb (Left a))
-    loopbwd (Right c) = c
+    loopbwd :: a + b -> b
+    loopbwd (InL a) = loopbwd (from comb (InL a))
+    loopbwd (InR c) = c
 
 
 not :: Iso' Bool
 not = swapCoprod
 
-just :: Iso b (Either () b)
-just = Iso Right (\(Right b) -> b)  -- intentionally partial
+just :: Iso b (U + b)
+just = Iso InR (\(InR b) -> b)  -- intentionally partial
 
 add1 :: Iso' Nat
-add1 = trans just fold
+add1 = just >> fold
 
 sub1 :: Iso' Nat
 sub1 = sym add1
 
-false :: Iso () Bool
+false :: U <=> Bool
 false = just
 
-true :: Iso () Bool
-true = trans just not
+true :: U <=> Bool
+true = just >> not
 
-right :: Iso a (Either a a)
+right :: Iso a (a + a)
 right = do
   sym unit
   parProd false id
   distrib
   parCoprod unit unit
 
-zero :: Iso () Nat
+zero :: U <=> Nat
 zero = trace $ do
   swapCoprod
   fold
   right
 
-isZero :: Iso' (Nat, Bool)
+isZero :: Iso' (Nat * Bool)
 isZero = do
   parProd (sym fold) id
   distrib
@@ -165,14 +166,14 @@ isZero = do
   sym distrib
   parProd fold id
 
-move1 :: Iso (Nat, Nat) (Either (Nat, Nat) Nat)
+move1 :: (Nat * Nat) <=> ((Nat * Nat) + Nat)
 move1 = do
   parProd (sym fold) id
   distrib
   parCoprod unit (parProd id add1)
   swapCoprod
 
-copoint :: Iso (Either a a) (a, Bool)
+copoint :: a + a <=> (a * Bool)
 copoint = do
   parCoprod (sym unit) (sym unit)
   sym distrib
@@ -184,19 +185,22 @@ debug msg = Iso (Dbg.trace msg)
                 (Dbg.trace ("~" Dbg.++ msg))
 
 main :: IO ()
-main = putStrLn $ show $ to (parProd (zero >> add1 >> add1) true >> isEven) ((), ())
-  where
-    bimap :: (a -> b) -> (c -> d) -> (a, c) -> (b, d)
-    bimap f g (a, c) = (f a ,g c)
+main = putStrLn "hello"
+
+-- main :: IO ()
+-- main = putStrLn $ show $ to (parProd (zero >> add1 >> add1) true >> isEven) (Pair U U)
+--   where
+--     bimap :: (a -> b) -> (c -> d) -> (a * c) -> (b * d)
+--     bimap f g (Pair a c) = Pair (f a) (g c)
 
 
-sw :: Iso (a, (b, c)) (b, (a, c))
+sw :: (a * (b * c)) <=> (b * (a * c))
 sw = do
   assocProd
   parProd swapProd id
   sym assocProd
 
-iterNat :: Iso' a  -> Iso' (Nat, a)
+iterNat :: Iso' a  -> Iso' (Nat * a)
 iterNat step = do
   sym unit
   trace $ do
@@ -208,6 +212,6 @@ iterNat step = do
     parCoprod (parProd id (parProd id step) >> sw) id
   unit
 
-isEven :: Iso' (Nat, Bool)
+isEven :: Iso' (Nat * Bool)
 isEven = iterNat not
 
